68e31c85f97ac399fb368747a0d6daf041da6a88
Brattlof <57799495+Brattlof@users.noreply.github.com>
2022-04-26 20:57:39 +0200

    Precompiled header

--- Noz/src/ECS/Entity/Entity.cpp
+++ Noz/src/ECS/Entity/Entity.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+#include "pch.hpp"
 #include "Entity.hpp"
 #include "../BaseComponent/BaseComponent.hpp"
 
--- Noz/src/Renderer/Application/Application.cpp
+++ Noz/src/Renderer/Application/Application.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+#include "pch.hpp"
 #include "Application.hpp"
 
 using namespace Noz;
