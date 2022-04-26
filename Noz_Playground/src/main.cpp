3214e02c583d7a560fe446d67dc2fe643183b967
Brattlof <57799495+Brattlof@users.noreply.github.com>
2022-04-26 22:39:00 +0200

    unique_ptr

--- Noz_Playground/src/main.cpp
+++ Noz_Playground/src/main.cpp
@@ -3,7 +3,7 @@
 
 int Entry(int argc, char** argv)
 {
-	Noz::Application* app = new Noz::Application(NOZ_OBFUSCATE("NOZ_PLAYGROUND"), 900, 600);
+	std::unique_ptr<Noz::Application> app = std::make_unique<Noz::Application>(NOZ_OBFUSCATE("NOZ_PLAYGROUND"), 900, 600);
 
 	NOZ_LOG_INFO(NOZ_OBFUSCATE("Num of GPU's: %d"), (int)app->GetVulkanAPI().GetGpuCount());
 
@@ -11,8 +11,6 @@ int Entry(int argc, char** argv)
 	{
 		app->Update();
 	}
-
-	delete app;
 
 	return 0;
 }
